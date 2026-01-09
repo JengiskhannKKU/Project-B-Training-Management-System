69ef6cc31df5c3861f2bcafb28212fc9f8533117
dollarsanchez <dolpeeradol@gmail.com>
2026-01-10 03:04:42 +0700

    feat(auth): make login link visible on all registration steps

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -408,26 +408,6 @@ const organizationLabel = computed(() => {
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
                         </svg>
                     </button>
-
-                    <div class="mt-6">
-                         <div class="relative">
-                            <div class="absolute inset-0 flex items-center">
-                                <div class="w-full border-t border-gray-300"></div>
-                            </div>
-                            <div class="relative flex justify-center text-sm">
-                                <span class="px-2 bg-white text-gray-500">{{ $t('Already have an account?') }}</span>
-                            </div>
-                        </div>
-                        <div class="mt-4 text-center">
-                             <Link
-                                :href="route('login')"
-                                @click="clearStorage"
-                                class="font-medium text-[#3D9792] hover:text-[#2d7773]"
-                            >
-                                {{ $t('Log in') }}
-                            </Link>
-                        </div>
-                    </div>
                 </div>
 
                 <!-- Form Steps -->
@@ -651,6 +631,26 @@ const organizationLabel = computed(() => {
                         </PrimaryButton>
                     </div>
                 </form>
+
+                <div class="mt-6">
+                        <div class="relative">
+                        <div class="absolute inset-0 flex items-center">
+                            <div class="w-full border-t border-gray-300"></div>
+                        </div>
+                        <div class="relative flex justify-center text-sm">
+                            <span class="px-2 bg-white text-gray-500">{{ $t('Already have an account?') }}</span>
+                        </div>
+                    </div>
+                    <div class="mt-4 text-center">
+                            <Link
+                            :href="route('login')"
+                            @click="clearStorage"
+                            class="font-medium text-[#3D9792] hover:text-[#2d7773]"
+                        >
+                            {{ $t('Log in') }}
+                        </Link>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
